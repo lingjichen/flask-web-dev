85dcdeeeb356531f3465ff48002e164a9ec55a91
lingjichen <499910843@qq.com>
2017-03-15 15:20:38 +0800

    day 6

--- hello.py
+++ hello.py
@@ -1,13 +1,113 @@
-from flask import Flask
+import os
+from datetime import datetime
+from flask import Flask,render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_script import Manager, Shell
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail,Message
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.qq.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PERFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
-@app.route('/')
+manager = Manager(app)
+boorstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+mail = Mail(app)
+
+#
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True)
+    user = db.relationship('User', backref = 'role', lazy = 'dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique = True, index = True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PERFIX'] + subject,
+                  sender = app.config['FLASKY_MAIL_SENDER'], recipients = [to])
+    msg.body = render_template(template + '.txt.', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
+
+
+#自动导入程序、数据库实例、模型
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command('shell',Shell(make_context=make_shell_context))
+manager.add_command('db',MigrateCommand)
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators = [Required()])
+    submit = SubmitField('Submit')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
+
+@app.route('/',methods =['GET','POST'])
 def index():
-    return '<h1>Hello world</h1>'
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filterby(username = form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user = user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',
+                           current_time=datetime.utcnow(), form =form, name=session.get('name'),
+                           known = session.get('known',False))
 
+"""
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello %s</h1>' % name
+    return render_template('user.html',name=name)
+"""
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
